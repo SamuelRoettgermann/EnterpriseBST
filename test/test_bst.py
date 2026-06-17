12bb1523c5b7e5c6a34fb8dd09cfc2c2708f255c
Samuel Rötttgermann <samu.roettgermann@t-online.de>
2026-06-18 01:07:10 +0200

    added tests for for-loops

--- test/test_bst.py
+++ test/test_bst.py
@@ -1,12 +1,26 @@
+import random
+import sys
 import unittest
 from bst import BST
 
 
 class TestBST(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        sys.setrecursionlimit(10_000_000)
+
     def setUp(self):
         """Create a fresh BST before each test."""
         self.bst = BST[int]()
+
+    def _set_up_int_tree(self, size: int):
+        """Used to obtain a somewhat non-deformed tree"""
+        insert_sequence = list(range(size))
+        random.shuffle(insert_sequence)
+
+        for value in insert_sequence:
+            self.bst.insert(value)
 
     def test_empty_tree(self):
         """An empty tree should not find any values."""
@@ -93,6 +107,50 @@ class TestBST(unittest.TestCase):
         self.bst.insert(10)
         self.assertTrue(self.bst.remove(10))
         self.assertFalse(self.bst.search(10))
+
+    def test_for_loop_single_element(self):
+        self.bst.insert(10)
+
+        for value in self.bst:
+            self.assertEqual(value, 10)
+
+    def test_for_loop_multiple_elements(self):
+        self._set_up_int_tree(100)
+
+        i = 0
+        for value in self.bst:
+            self.assertEqual(value, i)
+            i += 1
+
+    def test_iterators_do_not_influence_other_iterators(self):
+        self._set_up_int_tree(100)
+
+        i = 0
+        for value in self.bst:
+            self.assertEqual(value, i)
+            i += 1
+
+            j = 0
+            for inner_value in self.bst:
+                self.assertEqual(inner_value, j)
+                j += 1
+
+
+    def test_enumerate(self):
+        self._set_up_int_tree(1000)
+
+        for i, value in enumerate(self.bst):
+            self.assertEqual(value, i)
+
+    def test_zip(self):
+        self._set_up_int_tree(1000)
+
+        for i, value in zip(range(1000), self.bst):
+            self.assertEqual(value, i)
+
+    def test_empty_iterator(self):
+        for _ in self.bst:
+            self.assertFalse(True)
 
 
 if __name__ == "__main__":
