75ff3d111cdb478aa89cc0b1bb4d33e734689f5b
Samuel Rötttgermann <samu.roettgermann@t-online.de>
2026-06-21 19:24:11 +0200

    added basic wrapper implementation for Native tree

--- src/trees/native/native.py
+++ src/trees/native/native.py
@@ -2,4 +2,40 @@ from trees import Tree
 
 
 class Native[T](Tree[T]):
-    pass
+    _data: set[T]
+
+    def __init__(self):
+        self._data = set()
+
+    def insert(self, value: T):
+        self._data.add(value)
+
+    def search(self, value: T) -> bool:
+        return value in self._data
+
+    def remove(self, value: T) -> bool:
+        if value in self._data:
+            self._data.remove(value)
+            return True
+
+        return False
+
+    def depth(self) -> int:
+        return bool(self._data)
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __str__(self) -> str:
+        if not self._data:
+            return "Empty Tree"
+
+        output = [f"├── {value}" for value in self._data]
+        output[-1] = f"└{output[1:]}"
+
+        output[0:0] = ["Root"]
+
+        return "\n".join(output)
+
+    def __iter__(self):
+        yield from self._data
