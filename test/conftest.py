45688916743c0c41c53479b6845f70557513066d
Samuel Rötttgermann <samu.roettgermann@t-online.de>
2026-06-23 04:04:03 +0200

    registered listwrapper for tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,15 @@
 import sys
 import pytest
 
-from trees import BST, AVL, SetWrapper
+from trees import *
 
 sys.setrecursionlimit(1_000_000)
 
 
-@pytest.fixture(params=[BST, AVL, SetWrapper], ids=["BST", "AVL", "SetWrapper"])
+@pytest.fixture(
+    params=[BST, AVL, SetWrapper, ListWrapper],
+    ids=["BST", "AVL", "SetWrapper", "ListWrapper"],
+)
 def tree_factory(request):
     """Provides the class constructors for the trees under test."""
     return request.param
