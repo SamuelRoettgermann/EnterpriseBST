7da5dd43616204330cd7a3bad6266249a0418d3b
Samuel Rötttgermann <samu.roettgermann@t-online.de>
2026-06-24 02:36:53 +0200

    initialized __init__ files

--- src/trees/__init__.py
+++ src/trees/__init__.py
@@ -4,5 +4,6 @@ from .bst import BST
 from .avl import AVL
 from .setwrapper import SetWrapper
 from .listwrapper import ListWrapper
+from .trie import Trie
 
-__all__ = ["Tree", "BST", "AVL", "SetWrapper", "ListWrapper"]
+__all__ = ["Tree", "BST", "AVL", "SetWrapper", "ListWrapper", "Trie"]
