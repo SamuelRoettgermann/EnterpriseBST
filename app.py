b5e6332a0078f8f35dd3ae61a94252f506d8c7ca
Sunny Godara <216120834+Sunny-Godara@users.noreply.github.com>
2026-06-25 12:41:44 +0530

    feat: implement working flask endpoint for running bst benchmarks

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+@app.get('/benchmark_test')
+def benchmark_test():
+    return render_template('benchmark_page.html')
+
+
+@app.get('/benchmark_test_result')
+def benchmar_test_result():
+    result = subprocess.run(['pytest' , '--benchmark-only', '-k','bst'] , capture_output = True, text = True) 
+
+    return f'{result.stdout}'
+                      
