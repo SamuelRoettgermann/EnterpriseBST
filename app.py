8641cbca2e4bbbc93c9755cfb4040805f72c4578
Sunny Godara <216120834+Sunny-Godara@users.noreply.github.com>
2026-06-26 20:13:40 +0530

    ran black

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 
 @app.get("/")
 def benchmark_navigation():
-    """ Shows the main webpage with the button to get the benchmark resutlts"""
+    """Shows the main webpage with the button to get the benchmark resutlts"""
     return render_template("benchmark_page.html")
 
 
 @app.get("/benchmark_bst_results")
 def benchmark_bst_results():
-
     """Runs the pytest benchmark command for the BST and returns the text output"""
     result = subprocess.run(
         ["pytest", "--benchmark-only", "-k", "bst"], capture_output=True, text=True
