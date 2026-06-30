b3bd1bf35b0e8389587cb7cf304c02fb0de09a2d
Samuel Rötttgermann <samu.roettgermann@t-online.de>
2026-06-30 20:54:47 +0200

    removed B+-tree from /src/init

--- src/trees/__init__.py
+++ src/trees/__init__.py
@@ -5,6 +5,5 @@ from .avl import AVL
 from .setwrapper import SetWrapper
 from .listwrapper import ListWrapper
 from .trie import Trie
-from .bplus import BPlusTree
 
-__all__ = ["Tree", "BST", "AVL", "SetWrapper", "ListWrapper", "Trie", "BPlusTree"]
+__all__ = ["Tree", "BST", "AVL", "SetWrapper", "ListWrapper", "Trie"]
